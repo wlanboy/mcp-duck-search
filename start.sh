37165f2ee1be80fdb32282142273843bbc312b10
wlanboy <wlanboy@github.com>
2026-03-14 14:13:16 +0100

    fixed container name

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ docker run -d --name searxng \
   -v ./searxng:/etc/searxng \
   searxng/searxng
 
-docker stop mcp-duck-search || true
-docker rm  mcp-duck-search || true
+docker stop mcp-duck || true
+docker rm  mcp-duck || true
 
 docker build -t mcp-duck-search .
 docker run -d --name mcp-duck -p 9900:9900 -e SEARXNG_URL=http://gmk:8880 mcp-duck-search
