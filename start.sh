72f93e01bdd6342e0573fe3960e1d05d940159fa
wlanboy <wlanboy@github.com>
2026-03-14 14:10:57 +0100

    added searxng container

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
-docker stop  mcp-duck-search  || true
-docker rm  mcp-duck-search  || true
+docker pull searxng/searxng
+
+docker stop searxng || true
+docker rm searxng || true
+
+docker run -d --name searxng \
+  -p 8880:8080 \
+  -v ./searxng:/etc/searxng \
+  searxng/searxng
+
+docker stop mcp-duck-search || true
+docker rm  mcp-duck-search || true
 
 docker build -t mcp-duck-search .
-docker run -d --name mcp-duck -p 9900:9900 mcp-duck-search
+docker run -d --name mcp-duck -p 9900:9900 -e SEARXNG_URL=http://gmk:8880 mcp-duck-search
