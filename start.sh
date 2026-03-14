85d8ed1d961307547564b65ef3998803a28fccdd
wlanboy <wlanboy@github.com>
2026-03-14 14:15:26 +0100

    added network

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
+docker network create mcp-net 2>/dev/null || true
+
 docker pull searxng/searxng
 
 docker stop searxng || true
 docker rm searxng || true
 
 docker run -d --name searxng \
+  --network mcp-net \
   -p 8880:8080 \
   -v ./searxng:/etc/searxng \
   searxng/searxng
@@ -12,4 +15,8 @@ docker stop mcp-duck || true
 docker rm  mcp-duck || true
 
 docker build -t mcp-duck-search .
-docker run -d --name mcp-duck -p 9900:9900 -e SEARXNG_URL=http://gmk:8880 mcp-duck-search
+docker run -d --name mcp-duck \
+  --network mcp-net \
+  -p 9900:9900 \
+  -e SEARXNG_URL=http://searxng:8080 \
+  mcp-duck-search
